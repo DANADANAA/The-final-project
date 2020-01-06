b44dc2f7f02201a69b1856cfd6d9fa4858f8ba3c
DANADANAA <59542940+DANADANAA@users.noreply.github.com>
2020-01-06 12:47:19 +0200

    Create types.h

---
+++ types.h
@@ -0,0 +1,8 @@
+# The-final-project# ifndef TYPES_H
+# define  TYPES_H
+
+typedef  غير موقعة  char uint8؛
+typedef  uint16 قصيرة غير موقعة  ؛
+typedef و  غير موقعة  كثافة العمليات uint32.
+
+# endif
